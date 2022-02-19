405133f7e329f5ea587add6a8f095bbd48b499d4
Jeanet Lin <linett@gmail.com>
2022-02-19 11:49:46 +0100

    attempt on displaying food input from form

--- routes/nutri.routes.js
+++ routes/nutri.routes.js
@@ -2,18 +2,34 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const Diary = require("../models/diary.model");
 const { isLoggedIn } = require("../middlewares/guard");
+const { redirect } = require("express/lib/response");
 
 //Nutrition page
-router.get("/tracknutrition", (req, res) => {
-    const data = {}
-  res.render("nutrition", {data});
+router.get("/tracknutrition", async (req, res) => {
+  const data = await Diary.find({
+    user: req.session.currentUser._id,
+    date: req.body.date,
+  });
+  //data.map()
+  res.render("nutrition", { data });
 });
 
 router.post("/tracknutrition", isLoggedIn, async (req, res) => {
   const diary = new Diary();
   diary.user = req.session.currentUser._id;
   diary.date = req.body.date;
-  diary.breakfast.push({ test: "Test" });
+  //console.log(req.body)
+  if (req.body.meal == "breakfast") {
+  diary.breakfast.push({
+    foodtype: req.body.foodType,
+    portion: req.body.portion,
+    portionunit: req.body.portionUnit,
+    calories: req.body.calories,
+    carbs: req.body.carbs,
+    fat: req.body.fat,
+    protein: req.body.protein
+  });
+ }
   try {
     await diary.save();
 
@@ -25,7 +41,7 @@ router.post("/tracknutrition", isLoggedIn, async (req, res) => {
     res.render("nutrition", { data });
   } catch (error) {
     console.log("error");
-  } 
+  }
   //     const diary = Diary()
   //     diary.date = req.body.date
   //     diary.user = req.session._userid
